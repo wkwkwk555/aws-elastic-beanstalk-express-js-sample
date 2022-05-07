7611f93758cd711a8d9adf548c05a00a92fa21c3
wkwkwk555 <86640741+wkwkwk555@users.noreply.github.com>
2022-05-07 17:05:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('TEMPM'));
+app.get('/', (req, res) => res.send('CHANG STAGE REVIEW'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
